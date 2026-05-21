6089edf530953839976a0477b1eea6475de0b8b8
nayilalmuzaffar <nayilalmuzaffar@gmail.com>
2026-05-22 02:57:24 +0300

    Add files via upload

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_VITE_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_VITE_SUPABASE_ANON_KEY as string;
+const supabaseUrl = (process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.NEXT_PUBLIC_VITE_SUPABASE_URL || 'https://supabase.co') as string;
+const supabaseAnonKey = (process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_VITE_SUPABASE_ANON_KEY || 'placeholder') as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
